2f2e5bd7bb16e7f0ff90417ad7fb1412fe2d7566
Elad Ben-Israel <benisrae@amazon.com>
2020-05-25 21:36:06 +0300

    chore: initial commit

---
+++ .projenrc.js
@@ -0,0 +1,20 @@
+const { TypeScriptLibraryProject, Semver } = require('projen');
+
+const project = new TypeScriptLibraryProject({
+  name: 'jsii-srcmak',
+  jsiiVersion: Semver.caret('1.5.0'),
+  description: 'generate source code in multiple languages from typescript',
+  repository: 'https://github.com/eladb/jsii-srcmak.git',
+  authorName: 'Elad Ben-Israel',
+  authorEmail: 'benisrae@amazon.com',
+  stability: 'experimental',
+  devDependencies: {
+    '@types/node': Semver.caret('10.0.0')
+  },
+  dependencies: {
+    'jsii': Semver.pinned('1.1.0'),
+    'jsii-pacmak': Semver.pinned('1.1.0'),
+  }
+});
+
+project.synth();
